440fc40890224196d52f02ef39a6d286f060ccc7
newoverguy <newoverguy@gmail.com>
2013-12-10 11:59:52 +0900

    add a sample data

--- groovy/csv_parsing_img_download_zipping.groovy
+++ groovy/csv_parsing_img_download_zipping.groovy
@@ -5,6 +5,21 @@ import java.util.zip.ZipOutputStream
 import java.util.zip.ZipEntry  
 import java.nio.channels.FileChannel
 
+/*
+sample data
+=======
+userId,extid,imageUrl,date
+newoverguy,ca941b07-fd75-4b0c,http://www.xvfxvf.net/test_1384G/IMG_1384610278384.png,2013-11-16 22:58:58.976
+newoverguy,ca941b07-fd75-4b0c,http://www.xvfxvf.net/test_1384609y_PNG/IMG_1384609715770.png,2013-11-16 22:52:13.723
+showmethe,b82782e8-8c95-4f5e,http://www.xvfxvf.net/test_137816NG/IMG_1378169987695.png,2013-09-03 10:06:48.953
+takeout,7b5f0c76-f076-4ece,http://www.xvfxvf.net/test_138042VbawO_JPEG/image.jpg,2013-09-29 17:32:12.102
+takeout,7b5f0c76-f076-4ece,http://www.xvfxvf.net/test_1380446kl_JPEG/image.jpg,2013-09-29 17:31:38.539
+takeout,7b5f0c76-f076-4ece,http://www.xvfxvf.net/test_1378207WsK2_JPEG/image.jpg,2013-09-03 20:27:49.372
+hamburg,7b5f0c76-f076-4ece,http://www.xvfxvf.net/test_15hLwwC_JPEG/image.jpg,2013-09-03 20:26:17.614
+kimkimkim,d3a4de17-e222-45,http://www.xvfxvf.net/test_137932t_PNG/IMG_1379321953655.png,2013-09-16 18:00:41.928
+==========
+*/
+
 def rootPath = '/Users/nhn/Downloads'
 //def filePath = rootPath + '/test_images_sample.csv'
 def filePath = rootPath + '/test_images.csv'
